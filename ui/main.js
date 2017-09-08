9e1a98c02f4883912dbe0e6f001ca7892c451eb0
rakzz <30324806+rakzz@users.noreply.github.com>
2017-09-08 16:16:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element= document.getElementById('main-text');
 element.innerHTML='New Value';
+
+var img=document.GetElementById("15");
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
