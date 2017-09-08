546fe96bcad71b412435a4571bdec215d33dfff5
rakzz <30324806+rakzz@users.noreply.github.com>
2017-09-08 16:27:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,10 @@ console.log('Loaded!');
 
 //Change the text of the main-text div
 var element= document.getElementById('main-text');
+
 element.innerHTML='New Value';
 
-var img=document.GetElementById("15");
+var img=document.GetElementById('15');
 var marginLeft=0;
 function moveRight() {
     marginLeft = marginLeft + 10;
