60d814aef41b135e1bc457c387f2dc64bae57c9a
rakzz <30324806+rakzz@users.noreply.github.com>
2017-09-08 17:16:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,10 @@
-console.log('Loaded!');
+var button = document.GetElementById('counter');
+var counter=0;
 
-//Change the text of the main-text div
-var element= document.getElementById('main-text');
-
-element.innerHTML='New Value';
-
-var img=document.getElementById('15');
-var marginLeft=0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-     img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick=function(){
-    var interval= setInterval(moveRight , 50);
+button.onclick=function () {
+    
+    var request = new XMLHttpRequest();
+    counter= counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
