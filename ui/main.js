07a52f2d09dcae5b58c1dcb9f4899c0b12364668
rakzz <30324806+rakzz@users.noreply.github.com>
2017-09-08 16:25:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ var element= document.getElementById('main-text');
 element.innerHTML='New Value';
 
 var img=document.GetElementById("15");
+var marginLeft=0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+     img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval= setinterval(moveRight , 100);
 };
