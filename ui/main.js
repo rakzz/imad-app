f13030b60d0ffddebf2bfa7d9b0b3abb91152641
rakzz <30324806+rakzz@users.noreply.github.com>
2017-09-18 09:36:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,15 @@
 var button = document.getElementById('counter');
+var counter=0;
 
 
-button.onclick=function () {
+button.onclick = function () {
     
     var request = new XMLHttpRequest();
     
-    request.onreadystatechange= function(){
-            if(request.readyState==XMLHttp.DONE){
-                if (request.status==200){
-                    var counter =request.responseText;
+    request.onreadystatechange = function(){
+            if (request.readyState == XMLHttp.DONE){
+                if (request.status == 200){
+                    var counter = request.responseText;
                     var span = document.getElementById('count');
                     span.innerHTML = counter.toString();
                  }
