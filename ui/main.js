cd49a48f6cbbad402206cf471eb6a3baa920d059
rakzz <30324806+rakzz@users.noreply.github.com>
2017-09-18 09:24:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,19 @@
 var button = document.getElementById('counter');
-var counter=0;
+
 
 button.onclick=function () {
     
     var request = new XMLHttpRequest();
-    counter= counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    request.onreadystatechange= function(){
+            if(request.readyState==XMLHttp.DONE){
+                if (request.status==200){
+                    var counter =request.response.Text;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                 }
+        }
+    };
+    request.open('GET','http://http://rakshandametkari03.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
